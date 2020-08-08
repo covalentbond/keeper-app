1e08b3bf5a9f897318104ba120d5092d22e3171f
Arijit Kundu <kunduarijit55@gmail.com>
2020-08-08 07:37:15 +0530

    Alternative of React useRefs

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,67 +1,152 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import AddIcon from "@material-ui/icons/Add";
 import { Fab } from "@material-ui/core";
 import { Zoom } from "@material-ui/core";
 
-function CreateArea(props) {
-  const [note, setNote] = useState({
+class CreateArea extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   // this.myRef = React.createRef();
+  state = {
     title: "",
-    content: ""
-  });
+    content: "",
+    isExpanded: false
+  };
+  // }
 
-  const [isExpanded, setIsExpanded] = useState(false);
+  expand = () => {
+    this.setState({ isExpanded: true });
+    // this.myRef.current.focus();
+  };
 
-  function expand() {
-    setIsExpanded(true);
-  }
-
-  function handleChange(event) {
+  handleChange = (event) => {
     const { name, value } = event.target;
 
-    setNote((prevNote) => {
+    this.setState((prevNote) => {
       return {
         ...prevNote,
         [name]: value
       };
     });
-  }
+  };
 
-  function submitNote(event) {
-    props.onAdd(note);
-    setNote({
-      title: "",
-      content: ""
-    });
+  submitNote = (event) => {
     event.preventDefault();
-  }
+    this.props.onAdd(this.state);
+    this.setState((prevNote) => {
+      return {
+        ...prevNote,
+        title: "",
+        content: ""
+        // isExpanded: true
+      };
+    });
+  };
 
-  return (
-    <div>
-      <form className="create-note">
-        {isExpanded && (
-          <input
+  render() {
+    return (
+      <div>
+        <form className="create-note">
+          <input //TODO: Make the focus back to Title
             name="title"
-            onChange={handleChange}
-            value={note.title}
+            onChange={this.handleChange}
+            value={this.state.title}
             placeholder="Title"
+            onClick={this.expand}
+            autoComplete="off"
+            required
           />
-        )}
-        <textarea
-          name="content"
-          onChange={handleChange}
-          value={note.content}
-          placeholder="Take a note..."
-          onClick={expand}
-          rows={isExpanded ? 3 : 1}
-        />
-        <Zoom in={isExpanded}>
-          <Fab onClick={submitNote}>
-            <AddIcon />
-          </Fab>
-        </Zoom>
-      </form>
-    </div>
-  );
+          {this.state.isExpanded && (
+            <textarea
+              name="content"
+              onChange={this.handleChange}
+              value={this.state.content}
+              placeholder="Take a note..."
+              autoComplete="off"
+              rows={3}
+              required
+            />
+          )}
+          <Zoom in={this.state.isExpanded}>
+            <Fab onSubmit={this.submitNote} onClick={this.submitNote}>
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreateArea;
+// import React, { useState } from "react";
+// import AddIcon from "@material-ui/icons/Add";
+// import { Fab } from "@material-ui/core";
+// import { Zoom } from "@material-ui/core";
+
+// function CreateArea(props) {
+//   const [note, setNote] = useState({
+//     title: "",
+//     content: ""
+//   });
+
+//   const [isExpanded, setIsExpanded] = useState(false);
+
+//   function expand() {
+//     setIsExpanded(true);
+//   }
+
+//   function handleChange(event) {
+//     const { name, value } = event.target;
+
+//     setNote((prevNote) => {
+//       return {
+//         ...prevNote,
+//         [name]: value
+//       };
+//     });
+//   }
+
+//   function submitNote(event) {
+//     props.onAdd(note);
+//     setNote({
+//       title: "",
+//       content: ""
+//     });
+//     event.preventDefault();
+//   }
+
+//   return (
+//     <div>
+//       <form className="create-note">
+//         {isExpanded && (
+//           <input
+//             name="title"
+//             onChange={handleChange}
+//             value={note.title}
+//             placeholder="Title"
+//             autoComplete="off"
+//           />
+//         )}
+
+//         <textarea //TODO: Make the focus back to Title
+//           name="content"
+//           onChange={handleChange}
+//           value={note.content}
+//           placeholder="Take a note..."
+//           onClick={expand}
+//           // onFocus
+//           rows={isExpanded ? 3 : 1}
+//           autoComplete="off"
+//         />
+//         <Zoom in={isExpanded}>
+//           <Fab onClick={submitNote}>
+//             <AddIcon />
+//           </Fab>
+//         </Zoom>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default CreateArea;
